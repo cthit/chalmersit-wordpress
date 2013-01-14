6a1cdf5dac077f5f3fecf1999fc7d7b7da2c8a5c
Johan Brook <johan.jvb@gmail.com>
2013-01-15 00:08:34 +0100

    Add tooltip to event meta

--- partials/_event-meta.php
+++ partials/_event-meta.php
@@ -1,6 +1,7 @@
 <?php 
 	global $post;
 	$host = get_post(get_post_meta($post->ID, IT_PREFIX."event_host", true));
+	$location = get_post_meta($post->ID, IT_PREFIX."event_location", true);
 ?>
 
 <ul class="meta event-meta">
@@ -8,9 +9,12 @@
 		<?php echo date("j F", strtotime(get_post_meta($post->ID, IT_PREFIX."event_date", true)));?>,
 		<?php echo get_post_meta($post->ID, IT_PREFIX."event_start_time", true);?>
 	-<?php echo get_post_meta($post->ID, IT_PREFIX."event_end_time", true);?></li>
-	<li class="icon-map-pin-fill"><?php echo get_post_meta($post->ID, IT_PREFIX."event_location", true);?></li>
 	
+	<?php if($location) : ?>
+	<li class="icon-map-pin-fill"><?php echo $location;?></li>
+	<?php endif;?>
+
 	<?php if($host):?>
-	<li class="icon-user"><a href="<?php echo get_permalink($host->ID);?>"><?php echo $host->post_title;?></a></li>
+	<li rel="tooltip" title="Kommittée som anordnar detta arrangemang" class="icon-user"><a href="<?php echo get_permalink($host->ID);?>"><?php echo $host->post_title;?></a></li>
 	<?php endif;?>
 </ul>
