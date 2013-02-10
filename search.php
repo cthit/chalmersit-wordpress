863dc16d0e067b31eb6aa00b0501fd4c571ac12a
Johan Brook <johan.jvb@gmail.com>
2013-02-10 17:21:12 +0100

    Implement user search

--- search.php
+++ search.php
@@ -1,52 +1,82 @@
 <?php
 	/* Search template */
 
+	$user_query = new WP_User_Query(array(
+		"search_columns" => array('user_login', 'user_email', 'user_nicename' ),
+		"search" => "*". $_GET['s'] ."*"
+	));
+
 	get_header();
 ?>
 
-<section class="main-col six columns">
-	<div class="box">
-	<header>
-		<h1>Sökresultat</h1>
-		<p class="annotation">Sökord: "<?php the_search_query();?>"</p>
+<section class="main-col eight columns push-two">
+	<div class="box row">
+		<header>
+			<h1>Sökresultat</h1>
+			<?php get_search_form();?>
+		</header>
 
-		<?php get_search_form();?>
-	</header>
+		<section class="six columns">
 
-	<?php if(have_posts()) : ?>
-	
-	<ol class="list">
+			<h2>Nyheter och sidor</h2>
 
-	<?php while(have_posts()) : the_post(); ?>
+			<?php if(have_posts()) : ?>
+			
+			<ol class="simple-list">
 
-		<li>
-			<article role="article">
-				<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-				<div class="article-content">
-					<?php the_excerpt();?>
-				</div>
-			</article>
-		</li>
+			<?php while(have_posts()) : the_post(); ?>
 
-	<?php endwhile;?>
-	</ol>
+				<li>
+					<a href="<?php the_permalink();?>"><?php the_title();?></a>
+				</li>
 
-	<?php else : ?>
+			<?php endwhile;?>
+			</ol>
 
-	<p class="no-content">Inga träffar</p>
+			<?php else : ?>
 
-	<?php endif;?>
+			<p class="no-content">Inget hittades</p>
+
+			<?php endif;?>
+		</section>
+
+		<section class="six columns">
+
+			<h2>Personer</h2>
+
+			<ul class="list user-search-results">
+			<?php if($user_query->results) : ?>
+				<?php foreach($user_query->results as $user) : ?>
+				<?php $meta = get_user_meta($user->ID);?>
+
+				<li>
+					<?php echo get_avatar($user->ID, 64);?>
+
+					<dl>
+						<dt>Namn</dt>
+						<dd><?php user_fullname($user);?></dd>
+
+						<dt>E-post</dt>
+						<dd><a href="mailto:<?php echo $user->user_email;?>"><?php echo $user->user_email;?></a></dd>
+
+						<?php if($meta['it_year'][0]) : ?>
+						<dt>Antagningsår</dt>
+						<dd><?php echo $meta['it_year'][0];?></dd>
+						<?php endif;?>
+					</dl>
+				</li>
+
+			<?php endforeach; ?>
+
+			<?php else : ?>
+
+			<p class="no-content">Inga personer hittades</p>
+
+			<?php endif;?>
+			</ul>
+		</section>
+
 	</div>
 </section>
-
-<aside class="three columns sidebar">
-	<section class="box">
-		<h2>Mer</h2>
-	</section>
-</aside>
-
-<aside role="complementary" class="sidebar three columns">
-	<?php get_sidebar();?>
-</aside>
 
 <?php get_footer();?>
--- searchform.php
+++ searchform.php
@@ -1,3 +1,4 @@
 <form role="search" action="<?php echo get_option("home") ?>" method="get">
-	<input type="search" placeholder="Sök på Chalmers.it" results="0" name="s" />
+	<input type="search" placeholder="Sök på Chalmers.it" results="0" name="s"
+	<?php if(is_search()) : ?>value="<?php the_search_query();?>" <?php endif;?> />
 </form>
