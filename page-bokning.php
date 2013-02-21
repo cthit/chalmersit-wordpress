610307edc11f1df27f34bb7ed29c6a62ef4d3816
Johan Brook <johan.jvb@gmail.com>
2013-02-21 01:12:35 +0100

    Add content about bookings

--- page-bokning.php
+++ page-bokning.php
@@ -2,6 +2,9 @@
 	get_header(); 
 	the_post();
 
+	if(!is_user_logged_in()) auth_redirect();
+
+	global $current_user;
 	$subtitle = get_post_meta($post->ID, IT_PREFIX."subtitle", true);
 ?>
 
@@ -25,17 +28,47 @@
 
 <aside class="three columns pull-six side-nav">
 	<div class="box">
-		<h3>Om bokningssystemet</h3>
+		<h2>Om bokningssystemet</h2>
 
-		<p>Randomfakta om bokningar</p>
+		<h3>Hubben</h3>
+		<dl>
+			<dt>Kan bokas av</dt>
+			<dd>Förening</dd>
+			<dt>Tid</dt>
+			<dd>17+ på vardagar. 00-24 på helger</dd>
+		</dl>
+
+		<h3>Grupprummet</h3>
+		<dl>
+			<dt>Kan bokas av</dt>
+			<dd>Privat och förening</dd>
+			<dt>Tid</dt>
+			<dd>12-13, 17+ på vardagar. 00-24 på helger</dd>
+		</dl>
+
+		<p>
+			P.R.I.T. reserverar sig för att ta bort eller ändra bokningar.
+		</p>
 	</div>
 </aside>
 
 <aside class="three columns sidebar">
-	<?php if(get_post_meta($post->ID, IT_PREFIX."is_committee", true)) : ?>
-	<?php get_sidebar("committee");?>
-	<?php endif;?>
-	<?php get_sidebar();?>
+	<div class="box">
+		<h3>Tidigare bokningar</h3>
+
+		<?php $bookings = get_bookings_for_user($current_user->ID);?>
+
+		<ul class="simple-list">
+		<?php foreach($bookings as $b) : ?>
+			<li>
+				<h4><?php echo $b->title;?></h4>
+				<p class="meta"><?php echo $b->location;?> <?php sep();?>
+					<?php echo date("Y-m-d H:i", strtotime($b->start_time));?> – <?php echo date("Y-m-d H:i", strtotime($b->end_time));?>
+				</p>
+			</li>
+		<?php endforeach;?>
+		</ul>
+	</div>
 </aside>
 
 <?php get_footer(); ?>
