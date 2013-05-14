95a2381a686cb4e15ec034cb102da65a5db3d1ca
Emil Edholm <emil@edholm.it>
2013-05-14 17:53:40 +0200

    Reverted multiple file upload commit

--- lib/inc.print.php
+++ lib/inc.print.php
@@ -6,24 +6,17 @@ require_once "Snoopy/Snoopy.class.php";
 ini_set("upload_max_filesize", "100M");
 ini_set("post_max_file", "100M");
 
-function printer($user, $pass, $printer, $files, $one_sided = true, $copies = 1, $range='') {
+function printer($user, $pass, $printer, $file, $one_sided = true, $copies = 1, $range='') {
     $pass=stripcslashes($pass);
 	if($con = ssh2_connect("remote1.studat.chalmers.se", 22)){
-		if(ssh2_auth_password($con, $user, $pass)) {
+		if(ssh2_auth_password($con, $user, $pass)){
 			ssh2_exec($con, "mkdir -p .print");
-
-			exec("tar cfz chalmersit.tar.gz" . escapeshellarg($files));
-			echo ("tar cfz chalmersit.tar.gz" . escapeshellarg($files));
-
-			ssh2_scp_send($con, "chalmersit.tar.gz", ".print/", 0644);
-			ssh2_exec($con, "tar -C .print/files -zxf chalmersit.tar.gz");
+			ssh2_scp_send($con, $file, ".print/chalmersit.dat", 0644);
 			$sides = (!$one_sided ? "one-sided" : "two-sided-long-edge");
 			$range = (empty($range) ? "" : "-o page-ranges=$range");
 
             // The environment variable CUPS_GSSSERVICENAME=HTTP must be set, othewise kerberos throws unauthorized
-			//$stream = ssh2_exec($con, "CUPS_GSSSERVICENAME=HTTP;lpr -P $printer -# $copies -o sides=$sides $range .print/files/*");
-			echo ("CUPS_GSSSERVICENAME=HTTP;lpr -P $printer -# $copies -o sides=$sides $range .print/files/*");
-			ssh2_exec($con, "rm -rf .print/*");
+			$stream = ssh2_exec($con, "CUPS_GSSSERVICENAME=HTTP;lpr -P $printer -# $copies -o sides=$sides $range .print/chalmersit.dat");
             $errorStream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
             stream_set_blocking($errorStream, true);
             $outputError = stream_get_contents($errorStream);
@@ -69,34 +62,22 @@ $errors = array();
 $preErrorMsg = "Kunde inte skriva ut din fil. ";
 
 if(isset($_POST['print'])) {
-	if (!empty($_FILES)) {
-		$failed = false;
-		for ($i = 0; $i < count($_FILES["upload"]["type"]); $i++) {
-			if ( !(in_array($_FILES["upload"]["type"][$i], $file_types) && $_FILES["upload"]["size"][$i] < 100500000)) {
-				$failed = true;
-				$errors[] = "Kunde inte skriva ut fil: \"" . $_FILES["upload"]["name"][$i] ."\". Utskriften avbryts. Kontrollera dess filtyp och storlek";
-				delete_files();
-				break;
-			}
-		}
-		if (!$failed) {
-			$printerError = printer($_POST["user"], $_POST["pass"], $_POST["printer"], $_FILES["upload"]["tmp_name"], $_POST["oneSided"], intval($_POST['copies']), $_POST['ranges']);
-			if(empty($printerError)) {
-				$notice = "Din fil är utskriven!";
-			}
-			else {
-				$errors[] = "Kunde inte skriva ut din fil. " . $printerError;
-			}
-			delete_files();
-		}
-	} else {
-		$errors[] = "Kunde inte skriva ut filen. Kontrollera filtyp och storlek";
-	}
-}
 
-function delete_files() {
-	for ($i = 0; count($_FILES["upload"]["name"][$i]); $i++) {
-		@unlink($_FILES["upload"]["tmp_name"][$i]);
+	if(!empty($_FILES) && in_array($_FILES["upload"]["type"], $file_types) && $_FILES["upload"]["size"] < 100500000) {
+
+        $printerError = printer($_POST["user"], $_POST["pass"], $_POST["printer"], $_FILES["upload"]["tmp_name"], $_POST["oneSided"], intval($_POST['copies']), $_POST['ranges']);
+		if(empty($printerError)) {
+
+			$notice = "Din fil är utskriven!";
+			@unlink($_FILES["upload"]["tmp_name"]);
+		}
+		else {
+			$errors[] = "Kunde inte skriva ut din fil. " . $printerError;
+			@unlink($_FILES["upload"]["tmp_name"]);
+		}
+	}
+	else {
+		$errors[] = "Kunde inte skriva ut filen. Kontrollera filtyp och storlek";
 	}
 }
 
