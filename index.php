ca4f58a509ecb7754baec1772268b386eb9cdfa7
Max Witt <kirayatail@gmail.com>
2013-05-14 22:06:10 +0200

    Purged index and enabled the new widgets

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php 
-	$lunch_lectures = get_posts(array(
-		"category" => get_it_option("lunch_category"),
-		"posts_per_page" => 3
-	));
-
 	get_header(); 
 ?>
 
@@ -61,79 +56,15 @@
 
 	
 	<div class="three columns">
-		
 		<?php if(is_active_sidebar("index-mid")) : ?>
 			<?php dynamic_sidebar("index-mid"); ?>
-		<?php endif;?>
-
-		<?php $comments = get_comments(array("status" => "approve", "number" => 10)); ?>
-
-		<?php if($comments) : ?>
-		<section class="box latest-comments">
-			<header class="panel-header">
-				<h1>Senaste kommentarerna</h1>
-			</header>
-
-			<ul class="simple-list">
-				<?php foreach($comments as $comment) : ?>
-				<?php $p = get_post($comment->comment_post_ID);?>
-
-				<li>
-					<p class="comment-author">
-						<?php echo get_avatar($comment->user_id, 32);?>
-						<strong><?php echo $comment->comment_author;?></strong>
-					</p>
-					på <a href="<?php echo get_permalink($p->ID);?>#comment-<?php echo $comment->comment_ID;?>"><?php echo $p->post_title;?></a>
-					<time><?php echo human_time_diff(strtotime($comment->comment_date));?> ago</time>
-				</li>
-				<?php endforeach;?>
-			</ul>
-		</section>
 		<?php endif;?>
 	</div>
 
 	<div class="three columns">
-		<section class="upcoming box">
-			<header>
-				<h3>Kommande</h3>
-			</header>
-			
-			<?php if($lunch_lectures) : ?>
-			<h2 class="section-heading">Lunchföreläsningar</h2>
-			<ul class="list lunch-lectures">
-
-				<?php foreach($lunch_lectures as $lecture) : ?>
-				<?php $date = get_post_meta($lecture->ID, IT_PREFIX."lunch_lecture_date", true);?>
-				<li>
-					<h3><?php echo $lecture->post_title;?></h3>
-					<ul class="meta">
-						<li><time datetime="<?php echo $date;?>">
-							<?php echo date("j F", strtotime($date));?>,
-							<?php echo get_post_meta($lecture->ID, IT_PREFIX."lunch_start_time", true);?></time>
-						</li>
-						<li>
-							<?php echo get_post_meta($lecture->ID, IT_PREFIX."lunch_lecture_location", true);?>
-						</li>
-						<li><?php the_author_posts_link(); ?></li>
-						<li>
-							<a href="<?php echo get_permalink($lecture->ID);?>" class="read-more">Läs mer</a>
-						</li>
-					</ul>
-				</li>
-				<?php endforeach;?>
-
-			</ul>
-			<?php endif;?>
-			
-			<h2 class="section-heading">Evenemang</h2>
-			
-			<?php echo do_shortcode('[google-calendar-events id="1" type="list" max="3"]' ); ?>
-		</section>
-
 		<?php if(is_active_sidebar("index-right")) : ?>
 			<?php dynamic_sidebar("index-right"); ?>
 		<?php endif;?>
-
 	</div>
 </section>
 
