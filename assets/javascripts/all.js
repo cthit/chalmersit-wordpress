251d5a337af86d799520287d41c6dfaa1ba935a1
Johan Brook <johan.jvb@gmail.com>
2013-05-19 20:51:57 +0200

    Regenerate JS and CSS

--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -1044,6 +1044,63 @@ $(function() {
 
 -------------------------------------------- */
 
+/*
+	jQuery Search and Replace plugin
+ */
+
+(function($) {
+
+	$.fn.replaceText = function(search, replace, text_only) {
+	return this.each(function() {
+	  var node = this.firstChild,
+	    val, new_val,
+
+	    // Elements to be removed at the end.
+	    remove = [];
+
+	  // Only continue if firstChild exists.
+	  if (node) {
+
+	    // Loop over all childNodes.
+	    do {
+
+	      // Only process text nodes.
+	      if (node.nodeType === 3) {
+
+	        // The original node value.
+	        val = node.nodeValue;
+
+	        // The new value.
+	        new_val = val.replace(search, replace);
+
+	        // Only replace text if the new value is actually different!
+	        if (new_val !== val) {
+
+	          if (!text_only && /</.test(new_val)) {
+	            // The new value contains HTML, set it in a slower but far more
+	            // robust way.
+	            $(node).before(new_val);
+
+	            // Don't remove the node yet, or the loop will lose its place.
+	            remove.push(node);
+	          } else {
+	            // The new value contains no HTML, so it can be set in this
+	            // very fast, simple way.
+	            node.nodeValue = new_val;
+	          }
+	        }
+	      }
+
+	    } while (node = node.nextSibling);
+	  }
+
+	  // Time to remove those elements!
+	  remove.length && $(remove).remove();
+	});
+	}
+
+})(jQuery);
+
 var Chalmers = (function(it) {
 	var root = it || {};
 
@@ -1276,6 +1333,10 @@ $(function() {
 			offset: parseInt($(ele).data('tooltip-offset')) || 0
 		});
 	};
+
+	// Byt ut alla förekomster av 'sexIT' till 'SEXIT' med större storlek på 'IT'
+	// så Anno slutar gnälla ...
+	$("h1, h2, p, li, a").replaceText(/sexit/gi, '<span class="sexit">sex<strong>IT</strong></span>');
 
 	// Add support for touch devices for the 'Tools' main nav menu
 	if("ontouchstart" in document) {
