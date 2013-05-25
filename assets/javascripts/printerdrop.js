1dea608b6a8ee28107ff35fec3931c0ee639b10f
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-05-26 00:14:14 +0200

    Matches with contains

--- assets/javascripts/printerdrop.js
+++ assets/javascripts/printerdrop.js
@@ -2,11 +2,9 @@ if (printerpage) {
 	$(function() {
 		$('#printer').click(function(){this.select();});
 		$('#printer').autocomplete({source: function(req, callback) {
-			var re = $.ui.autocomplete.escapeRegex(req.term);
-			var matcher = new RegExp('^' + re, "i");
 			var maxNum = 15;
 			var a = $.grep(printers, function(item) {
-				var matches = matcher.test(item);
+				var matches = req.test(item);
 				if (matches && maxNum-- < 0) return false;
 				return matches;
 			});
