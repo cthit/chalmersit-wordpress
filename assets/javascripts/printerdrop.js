4114d5d06e6c7f9a05bfdc44c4c13f120b3c3c1c
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-05-26 00:25:08 +0200

    Tweaked matching

--- assets/javascripts/printerdrop.js
+++ assets/javascripts/printerdrop.js
@@ -2,9 +2,11 @@ if (printerpage) {
 	$(function() {
 		$('#printer').click(function(){this.select();});
 		$('#printer').autocomplete({source: function(req, callback) {
+			var re = $.ui.autocomplete.escapeRegex(req.term);
+			var matcher = new RegExp(re, "i");
 			var maxNum = 15;
 			var a = $.grep(printers, function(item) {
-				var matches = req.test(item);
+				var matches = matcher.test(item);
 				if (matches && maxNum-- < 0) return false;
 				return matches;
 			});
