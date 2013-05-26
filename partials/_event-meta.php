6d8077e3e855735cfbabf4dd6b802d7a65fdcf3c
Johan Brook <johan.jvb@gmail.com>
2013-05-27 00:05:22 +0200

    Show Facebook event URL for event post meta

--- partials/_event-meta.php
+++ partials/_event-meta.php
@@ -2,6 +2,8 @@
 	global $post;
 	$host_id = get_post_meta($post->ID, IT_PREFIX."event_host", true);
 	$is_full_day = get_post_meta($post->ID, IT_PREFIX."full_day_event", true);
+	$fb_event_url = get_post_meta($post->ID, IT_PREFIX."fb_event_url", true);
+	$location = get_post_meta($post->ID, IT_PREFIX."event_location", true);
 
 	if($host_id && $host_id != -1) {
 		$host = get_post($host_id);
@@ -10,12 +12,12 @@
 		$other_host = get_post_meta($post->ID, IT_PREFIX."event_host_other", true);
 	}
 	
-	$location = get_post_meta($post->ID, IT_PREFIX."event_location", true);
+	
 ?>
 
 <ul class="meta event-meta">
 	<li class="icon-clock">
-		<?php echo date("j F", strtotime(get_post_meta($post->ID, IT_PREFIX."event_date", true)));?>
+		<?php echo date("j M", strtotime(get_post_meta($post->ID, IT_PREFIX."event_date", true)));?>
 	<?php if($is_full_day != "on") : ?>,
 		<?php echo get_post_meta($post->ID, IT_PREFIX."event_start_time", true);?>
 	-<?php echo get_post_meta($post->ID, IT_PREFIX."event_end_time", true);?>
@@ -24,6 +26,10 @@
 	
 	<?php if($location) : ?>
 	<li class="icon-map-pin-fill"><?php echo $location;?></li>
+	<?php endif;?>
+
+	<?php if($fb_event_url) : ?>
+	<li class="icon-facebook-sign" rel="tooltip" title="Arrangemangets sida på Facebook"><a target="_blank" href="<?php echo $fb_event_url;?>">Facebook</a></li>
 	<?php endif;?>
 
 	<?php if($host_id) : ?>
