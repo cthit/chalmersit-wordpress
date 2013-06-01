0decb4e54e45b06ed8856e75f1f90c1bb25f34f1
Max Witt <kirayatail@gmail.com>
2013-06-01 03:20:39 +0200

    Applied the profile sidebar to the profile page

--- page.php
+++ page.php
@@ -37,10 +37,13 @@
 </aside>
 
 <aside class="three columns sidebar">
-	<?php if(get_post_meta($post->ID, IT_PREFIX."is_committee", true)) : 
-			get_sidebar("committee");
-		endif;
-		get_sidebar();?>
+	<?php if(get_post_meta($post->ID, IT_PREFIX."is_committee", true)) : ?>
+	<?php get_sidebar("committee");?>
+	<?php endif;?>
+	<?php if(is_page("profil")){
+		dynamic_sidebar("profile");
+	}
+	get_sidebar();?>
 </aside>
 
 <?php endif; ?>
