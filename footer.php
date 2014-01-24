199528323a619a6fd422f137d73552742873cc55
Pontus Malm <malm@chalmers.it>
2014-01-24 20:40:09 +0100

    Google Analytics

--- footer.php
+++ footer.php
@@ -145,6 +145,17 @@
 </script>
 <?php endif;?>
 
+<!-- Google stalker script -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-25874865-1', 'chalmers.it');
+ga('send', 'pageview');
+</script>
+
 <!-- Designed and built by Johan Brook on a Mac. digIT 12/13.  -->
 
 </body>
